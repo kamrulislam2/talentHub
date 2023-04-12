109db48d2cc0f1c0083c13d013abcc7c36c91a91
kamrulislam2 <kamrulislamABK@gmail.com>
2023-04-12 16:14:46 +0600

    Readme file added

---
+++ Readme.md
@@ -0,0 +1,22 @@
+- The best features of my project "TalentHub" website
+
+  - First of all its SPA based react website
+  - In the home page it contains the featured job collection,
+    where you can choose your favorite one.
+  - After clicking on "view details" you will redirect to the details page about the job
+  - There you will find apply now option if you choose to apply for the job
+  - After applying for the job, it will set to the database (local storage database),
+    and if you visit "Applied Jobs" page there you will find those job
+    which you have applied recently.
+  - In the same page you can filter or sort the job by their job type, i.e. remote/onsite
+  - There have two more new pages one contains some blogs and page name is blog,
+    another contains previous assignment result using radar chart and page name is statistics.
+  - One more thing, in case you put the wrong route link in the address bar after "/" then
+    you will redirect to error page, where I designed the error page using some kind of meme
+    material. Out of fun fact we know that we should professional regarding our projects.
+    So, please ignore the mere image 😁
+  - I designed the home page for mobile responsive also with footer.
+
+  \*\*\* And many thanks for reading my writings 😁
+
+  - Live site link:
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
+import { Link, useNavigate } from "react-router-dom";
+import { ArrowUturnLeftIcon, HomeIcon } from "@heroicons/react/24/solid";
 
 const Error = () => {
   const navigate = useNavigate();
@@ -21,13 +21,21 @@ const Error = () => {
       <h2 className="font-extrabold text-8xl text-gray-500 mb-4">Error 404</h2>
       <p className="font-bold text-4xl text-gray-400 mb-6">(Page not found)</p>
 
-      <button
-        onClick={handleGoBack}
-        className="font-extrabold text-lg text-white bg-purple-500 hover:bg-purple-800 py-2 px-4 rounded-lg inline-flex items-center gap-2 mb-10"
-      >
-        <ArrowUturnLeftIcon className="h-5 w-5" />
-        Go Back
-      </button>
+      <div className="inline-flex gap-2">
+        <button
+          onClick={handleGoBack}
+          className="font-extrabold text-lg text-white bg-purple-500 hover:bg-purple-800 py-2 px-4 rounded-lg inline-flex items-center gap-2 mb-10"
+        >
+          <ArrowUturnLeftIcon className="h-5 w-5" />
+          Go Back
+        </button>
+        <Link to="/">
+          <button className="font-extrabold text-lg text-white bg-purple-500 hover:bg-purple-800 py-2 px-4 rounded-lg inline-flex items-center gap-2 mb-10">
+            <HomeIcon className="h-5 w-5" />
+            Go Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
